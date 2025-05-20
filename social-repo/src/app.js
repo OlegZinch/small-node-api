72e2db8ea31f513afe1f57b303603cfde0d0c026
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-05-20 19:55:14 +0100

    added connection to DB

--- social-repo/src/app.js
+++ social-repo/src/app.js
@@ -1,11 +1,11 @@
-const express = required('express')
-const usersRoter = required('./routes/users')
+const express = require('express')
+const usersRouter = require('./routes/users')
 
 module.exports = () => {
   const app = express()
 
   app.use(express.json())
-  app.use(usersRoter)
+  app.use(usersRouter)
 
   return app
 }
