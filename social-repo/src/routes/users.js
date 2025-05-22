e5698460e729dfef512c05100b858203daf2ad46
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-05-22 13:35:15 +0100

    updated put request

--- social-repo/src/routes/users.js
+++ social-repo/src/routes/users.js
@@ -31,7 +31,17 @@ router.post('/users', async (req, res) => {
   res.json(user)
 })
 
-router.put('/users/:id', async (req, res) => {})
+router.put('/users/:id', async (req, res) => {
+  const { id } = req.params
+  const { username, bio } = req.body
+
+  const user = await UserRepo.update(id, username, bio)
+
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' })
+  }
+  res.json(user)
+})
 
 router.delete('/users/:id', async (req, res) => {})
 
